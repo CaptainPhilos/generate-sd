8554d316883778dd325c2c304802dea850703780
Hervé <herve@Herves-MacBook-Pro.local>
2018-04-25 00:36:13 +0200

    finish game name extraction. add launch game command.

--- source/10-autostart.sh
+++ source/10-autostart.sh
@@ -29,15 +29,17 @@ debug=
 root_usb_key="/Volumes/ROMS"
 #root_usb_key = "/media/usb0/"
 root_roms="roms"
+#system_list="/etc/remulation/es_systems.cfg"
+system_list="/Volumes/ROMS/roms/es_systems.cfg"
 
 # Name of the core that will be launched
-core_name=
+core_name=""
 
 # name of the game
-game_name=
+game_name=""
 
 # controller file
-controller_config_filename=
+controller_config_filename=""
 
 # Error Management #######################################################
 
@@ -47,6 +49,12 @@ function trace() {
   if ! [[ -z $debug ]]; then
     echo "[debug] " $tracemsg
   fi
+}
+
+function warning() {
+  local warningmsg=$1
+
+  echo "[WARN] " $warningmsg
 }
 
 function exitonerror() {
@@ -59,6 +67,8 @@ function exitonerror() {
 # Check functions ########################################################
 
 function check_directories() {
+  trace "##########################"
+  trace "Function check_directories"
 
   # Check if the USB Key is accessible
   trace "root_usb_key=${root_usb_key}"
@@ -83,30 +93,49 @@ function check_directories() {
 
 # Game Management #######################################################
 
+# get the name of the core
 function extract_machine_name() {
+  trace "##########################"
+  trace "Function extract_machine_name"
+
   local roms_directory="${root_usb_key}/${root_roms}"
 
+  # get the first directory in roms directory
   local machine_name=$(file ${roms_directory}/* | grep directory | cut -d':' -f1 | head -1)
   trace "Machine Name (first)= ${machine_name}"
   machine_name=$(basename ${machine_name})
   trace "Machine Name (first)= ${machine_name}"
 
+  # no core name, exit
   if [[ -z ${machine_name} ]]; then
     exitonerror "Le premier répertoire trouvé n'a pas de nom"
   fi
 
+  # core name founded
   core_name=${machine_name}
   trace "Machine name is ${core_name}"
+
+  #check core_name in /etc/remulation/es_systems.cfg
+  if [[ $(grep "<name>${core_name}</name>" "${system_list}" | wc -l) -eq 0 ]]; then
+    exitonerror "Le nom de la machine (${core_name}) n'est pas supporté par cette version de Retropie"
+  fi
 }
 
+# Get the name of the game
 function extract_game_name() {
+  trace "##########################"
+  trace "Function extract_game_name"
+
   local game_directory="${root_usb_key}/${root_roms}/${core_name}"
 
+  # get list of files in core directory
   local files_in_directory=$(find ${game_directory}/* -type f)
   trace "List of files in ${game_directory}= ${files_in_directory}"
+  # check number of file in core directory
   local nb_files_in_directory=$(find ${game_directory}/* -type f | wc -l)
   trace "Nb of files in ${game_directory}= ${nb_files_in_directory}"
 
+  #control the content of the directory
   if [[ -z ${files_in_directory} ]]; then
     exitonerror "Le répertoire de la machine ${game_directory} est vide"
   fi
@@ -114,23 +143,22 @@ function extract_game_name() {
     exitonerror "Le répertoire de la machine ${game_directory} ne contient pas les bons fichiers : fichier du jeu (parfois 2) + la configuration des Controllers"
   fi
 
-  trace "Controller configuration verification = ${controller_config_filename}"
-  trace "Game file verification = ${game_name}"
+  # if some file to examine, scan file list and examines each
+#  for file in $(find "${game_directory}"/* -type f);
+#  do
+  while read file; do
 
-  find "${game_directory}"/* -type f | while read file; do
-
-    trace "Controller configuration verification = ${controller_config_filename}"
-    trace "Game file verification = ${game_name}"
-
+    # extract filename and extension name
     local onlyFileName=$(basename ${file})
     local extension=${onlyFileName: -3}
     extension=$(echo $extension | tr [:upper:] [:lower:])
-    trace "file gotten : ${onlyFileName}"
+    trace "file : ${file}"
+    trace "file only : ${onlyFileName}"
     trace "extension gotten : ${extension}"
 
     case ${extension} in
 
-      # controller configuration file
+      # the extension founded shows it's a configuration file
       ${CONTROLLER_CONFIG_EXT})
         controller_config_filename=${file}
         trace "Controller configuration file found = ${controller_config_filename}"
@@ -142,26 +170,33 @@ function extract_game_name() {
 
         # if it's a CUE file, check that it's compatible with the current machine
         trace "Actual core name = ${core_name}"
-        trace "Searched in = ${MACHINE_NEED_CUE}"
-        echo '${MACHINES_NEED_CUE}'
-        echo "${MACHINES_NEED_CUE}" | grep "${core_name}" > /dev/null 2>&1
-        echo "${MACHINES_NEED_CUE}" | grep "${core_name}" | wc -l
-        check_if_machine_need_cue=$(echo "${MACHINES_NEED_CUE}" | grep "${core_name}" | wc -l)
-        trace "Machine Need Cue ? = ${check_if_machine_need_cue}"
 
-#        if [[ "${check_if_machine_need_cue}" -eq 0 ]]; then
-#          game_name=${file}
-#          trace "Game file found = ${game_name}"
-#        fi
+        # if the core takes part of the core needing a cue file as game
+        if echo "${MACHINE_NEED_CUE}" | grep -q "${core_name}"; then
+          game_name=${file}
+          trace "Game file found = ${game_name}"
+        fi
         ;;
 
+        *)
+        # Check it's not a core needing cue file
+        if ! echo "${MACHINE_NEED_CUE}" | grep -q "${core_name}"; then
+          game_name=${file}
+          trace "Game file found = ${game_name}"
+        fi
+        ;;
 
     esac
 
-    trace "Controller configuration verification = ${controller_config_filename}"
-    trace "Game file verification = ${game_name}"
+  done <<< "$(find "${game_directory}"/* -type f)"
 
-  done
+  if [[ -z "${controller_config_filename}" ]]; then
+    warning "Pas de fichier de configuration des controllers. Utilisation des configurations installées en /opt/retropie/configs/all/retroarch-joypads"
+  fi
+
+  if [[ -z "${game_name}" ]]; then
+    exitonerror "Pas de jeux trouvé en ${game_directory}."
+  fi
 }
 
 function summary() {
@@ -169,6 +204,16 @@ function summary() {
   echo "La machine à émuler = ${core_name}"
   echo "Le fichier de configuration des controlleurs = ${controller_config_filename}"
   echo "Le fichier de jeu = ${game_name}"
+}
+
+# Launching games #######################################################
+
+function launch_game() {
+  trace "##########################"
+  trace "Function launch_game"
+
+  trace "Launching /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ ${core_name} ${game_name}"
+  /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ "${core_name}" "${game_name}"
 }
 
 # Parameters Management #################################################
@@ -246,5 +291,7 @@ extract_machine_name
 extract_game_name
 
 summary
+
+launch_game
 
 exit 0
