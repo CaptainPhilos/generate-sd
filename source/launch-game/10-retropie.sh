88d8a6a8c184c4a6a16fda16a5ed92161516bec8
Hervé <herve@Herves-MacBook-Pro.local>
2018-12-09 21:22:36 +0100

    change location of python log file. Before nowhere, after in the /bin directory of the USB key close the python script

--- source/launch-game/10-retropie.sh
+++ source/launch-game/10-retropie.sh
@@ -6,16 +6,17 @@ if [ "`tty`" = "/dev/tty1" ] && [ "$USER" = "pi" ]; then
 
   # globals ################################################################
 
-  root_usb_key="/media/usb0/"
-  bash_bin_path="bin/launch-game.sh"
-  python_bin_path="bin/launch-game.py"
+  root_usb_key="/media/usb0/bin/"
+  bash_bin_file="launch-game.sh"
+  python_bin_file="launch-game.py"
 
   # If script file is found in USB Key, then launch it
-  if [[ -f "${root_usb_key}${python_bin_path}" ]]; then
+  if [[ -f "${root_usb_key}${python_bin_file}" ]]; then
     # bash version - incomplete
     #(bash "${root_usb_key}${bash_bin_path}" > /dev/null 2>&1)
     # python version
-    (python3 "${root_usb_key}${python_bin_path}" > /dev/null 2>&1)
+    cd "${root_usb_key}"
+    (python3 "${python_bin_file}" > /dev/null 2>&1)
   # otherwise, launch emulation station (maybe it's a bad idea)
   else
     bash "/opt/retropie/configs/all/autostart.sh"
