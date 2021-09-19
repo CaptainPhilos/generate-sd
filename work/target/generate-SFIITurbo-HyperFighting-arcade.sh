1bd9941e8421413abece29ea5904b290b413f906
Hervé Le Bouler <herve.lebouler@gmail.com>
2021-09-19 19:22:25 +0200

    end of SF II Turbo Hyper Fighting

--- work/target/generate-SFIITurbo-HyperFighting-arcade.sh
+++ work/target/generate-SFIITurbo-HyperFighting-arcade.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-echo "../../source/generate-image/generate-generic-image.sh -d -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip"
-#../../source/generate-image/generate-generic-image.sh -d -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip -c ../origin/game-to-install/snes/Mega\ World.cfg
-../../source/generate-image/generate-generic-image.sh -d -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip
-=======
-echo "../../source/generate-image/generate-generic-image.sh -d -e lr-fbalpha -c "../origin/game-to-install/arcade/Mega World.cfg" -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip"
+echo "../../source/generate-image/generate-generic-image.sh -d -e lr-fbalpha -c ../origin/game-to-install/arcade/Mega World.cfg -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip"
 ../../source/generate-image/generate-generic-image.sh -d -e lr-fbalpha -c "../origin/game-to-install/arcade/Mega World.cfg" -i ../origin/origin.img -o sf-ii-turbo-hyper-firghting-mame-specific.img -s ../origin/game-to-install/arcade/sf2hf.zip 
->>>>>>> 99dd36b (add game files, joypad configurations)
